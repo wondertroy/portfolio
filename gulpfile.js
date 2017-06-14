b6c63ff76240d9052f51b80e00990d4cfed18a81
Woody Hill <wondertrot@gmail.com>
2017-06-14 20:40:39 +1000

    added sections to process

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('minify-css', ['less'], function() {
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('js/grayscale.js')
+    return gulp.src('js/dark.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
